320545d934b932f3209f07e7ba80364f5d0bda2c
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-17 12:27:58 -0400

    thunder in the air

--- Kei_Aedan_LoopsArrays.js
+++ Kei_Aedan_LoopsArrays.js
@@ -80,6 +80,29 @@ const filteredNames = filter(myNames, function(name) {
   return name[0] === 'R';
 });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+//console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
+function hazardWarningCreator(typeOfWarning){
+  let warningCounter = 0;
+  return function(location) {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    if (warningCounter ===1){
+      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time today!`);
+    } else {
+      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`);
+    }
+  };
+}
+/*
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const floodWarning = hazardWarningCreator('Water on the Road');
+const thunderWarning = hazardWarningCreator('Lightning in the air');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+floodWarning('Main St and Centinela Ave');
+thunderWarning('Olympic Blvd and Pacific Ave');
+thunderWarning('Olympic Blvd and Main St');
+*/
